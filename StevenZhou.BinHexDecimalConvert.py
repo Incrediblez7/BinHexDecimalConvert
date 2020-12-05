6d44c30d245f4cd6e015e96f70f3c76431532595
Abdulrahman <abd.moh.yousef@gmail.com>
2020-12-05 15:15:02 +0200

    Tidied up the code a bit by declaring a variable containing the dashes instead of having them in the middle of the code.

--- StevenZhou.BinHexDecimalConvert.py
+++ StevenZhou.BinHexDecimalConvert.py
@@ -1,12 +1,13 @@
 #Steven Zhou
 #BinHexDecimalConvert
 #2018/6/19
+dashes = "-" * 123
 
-print("---------------------------------------------------------------------------------------------------------------------------")
+print(dashes)
 while True:
   print("Welcome to the Binary, Hexadecimal and Decimal Converter! \n")
-  Choice = input("What do you want to convert? \n - Binary to Decimal (type in 'bd') , \n - Hexadecimal to Decimal (type in 'hd') , \n - Decimal to Binary (type in 'db') , \n - Decimal to Hexadecimal (type in 'dh') , \n - Binary to Hexadecimal (type in 'bh') , \n - Hexadecimal to Binary (type in 'hb') ? ")
-  print("---------------------------------------------------------------------------------------------------------------------------")
+  Choice = input("What do you want to convert? \n - Binary to Decimal (type in 'bd') , \n - Hexadecimal to Decimal (type in 'hd') , \n - Decimal to Binary (type in 'db') , \n - Decimal to Hexadecimal (type in 'dh') , \n - Binary to Hexadecimal (type in 'bh') , \n - Hexadecimal to Binary (type in 'hb') ? \n")
+  print(dashes)
   x = 0 
   while x == 0:
     print("\n"*5)
@@ -16,39 +17,39 @@ while True:
       print("Your number is:",int(BinNumber ,2))
       x = 1
       print("\n"*5)
-      print("---------------------------------------------------------------------------------------------------------------------------")
+      print(dashes)
     if Choice == 'hd':
      HexNumber = input("Please type in the number you want to convert from hexadecimal to decimal:")
      print("\n")
      print("Your number is:",int(HexNumber ,16))
      x = 1
      print("\n"*5)
-     print("---------------------------------------------------------------------------------------------------------------------------")
+     print(dashes)
     if Choice == 'db':
       DecNumber = input("Please type in the number you want to convert from decimal to binary:")
       print("\n")
       print("Your number is:",bin(int(DecNumber)))
       x = 1
       print("\n"*5)
-      print("---------------------------------------------------------------------------------------------------------------------------")
+      print(dashes)
     if Choice == 'dh':
       DecNumber = input("Please type in the number you want to convert from decimal to hexadecimal:")
       print("\n")
       print("Your number is:",hex(int(DecNumber)))
       x = 1
       print("\n"*5)
-      print("---------------------------------------------------------------------------------------------------------------------------")
+      print(dashes)
     if Choice == 'bh':
       BinNumber = input("Please type in the number you want to convert from binary to hexadecimal:")
       print("\n")
       print("Your number is:",hex(int(BinNumber ,2)))
       x = 1
       print("\n"*5)
-      print("---------------------------------------------------------------------------------------------------------------------------")
+      print(dashes)
     if Choice == 'hb':
       HexNumber = input("Please type in the number you want to convert from hexadecimal to binary:")
       print("\n")
-      print("Your number is:",bin(int(HexNumber ,1 6)))
+      print("Your number is:",bin(int(HexNumber ,16)))
       x = 1
       print("\n"*5)
-      print("---------------------------------------------------------------------------------------------------------------------------")
+      print(dashes)
